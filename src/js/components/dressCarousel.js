a01f739562f6fcf5fe2f78569ab371c76084162e
Hanna Herasimuk <Hanna_Herasimuk@epam.com>
2019-02-06 01:45:06 +0300

    Move carousel tabs to section top and make tabs change color depending on (in)active state

--- src/js/components/dressCarousel.js
+++ src/js/components/dressCarousel.js
@@ -4,6 +4,7 @@ import DressService from "../services/dressService.js";
 class DressCarousel extends React.Component {
 	constructor(props) {
 		super(props);
+		this.mainThemeColor = "rgb(253, 146, 109)";
 		this.state = { 
 			"data" : [],
 			"tab": "0"
@@ -19,6 +20,9 @@ class DressCarousel extends React.Component {
 		if (this.state.tab == i) {
 			return;
 		}
+		let switchTabsBtns = document.getElementsByClassName("switch-tabs-panel")[0].children;
+		[].forEach.call(switchTabsBtns, (item) => {item.style.backgroundColor = "white"});
+		switchTabsBtns[i].style.backgroundColor = this.mainThemeColor;
 		switch(i) {
 		case "0":
 			new DressService("clothesCarousel").fetchData().then(data => {
@@ -70,13 +74,15 @@ class DressCarousel extends React.Component {
 	render() {
 		return (
 			<section className="clothes-carousel">
-				<nav>
+				<nav className="switch-tabs-panel">
 					<button onClick={() => this.handleClick("0")}>WHAT&#39;S HOT?</button>
 					<button onClick={() => this.handleClick("1")}>DESIGNERS</button>
 					<button onClick={() => this.handleClick("2")}>FEATURED</button>
 					<button onClick={() => this.handleClick("3")}>LATESET</button>
 				</nav>
-				{this.renderList()}
+				<div className="clothes-gallery">
+					{this.renderList()}
+				</div>
 			</section>
 		);
 	}
